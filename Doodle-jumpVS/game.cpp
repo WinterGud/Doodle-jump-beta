ece49a2a835617fe01f7c8ee4b81a6e8697988d7
WinterGud <orestpalanii3@gmail.com>
2024-01-15 14:59:38 +0200

    comit

--- Doodle-jumpVS/game.cpp
+++ Doodle-jumpVS/game.cpp
@@ -3,7 +3,9 @@
 
 int main(int argc, char* argv[])
 {
-	std::string path1 = "C:\\Users\\User\\Desktop\\Doodle-jumpVS\\x64\\Debug\\data\\back.png";
+	//я навіть строку добавити неможу ): 
+	// для devu
+	//std::string path1 = "C:\\Users\\User\\Desktop\\Doodle-jumpVS\\x64\\Debug\\data\\back.png";
 
 	
 
