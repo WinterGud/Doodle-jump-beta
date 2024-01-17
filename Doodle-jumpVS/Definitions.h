34b9f8861dd94c04cfd4fb41353b9bbcb5576ba8
WinterGud <orestpalanii3@gmail.com>
2024-01-17 11:02:09 +0200

    commit_9

--- Doodle-jumpVS/Definitions.h
+++ Doodle-jumpVS/Definitions.h
@@ -9,6 +9,6 @@ static const int mapHight = 800;
 const int moveMapDown = mapHight / 8;
 
 
-const std::string doodlePath = "..\\..\\Doodle-jumpVS\\x64\\Debug\\data\\ghost-right-odskok@2x.png";
-const std::string holePath = "..\\..\\Doodle-jumpVS\\x64\\Debug\\data\\ghost-darkness@2x.png";
-const std::string standardPlatformPath = "..\\..\\Doodle-jumpVS\\x64\\Debug\\data\\bck@2x.png";
+const std::string doodlePath = "..\\x64\\Debug\\data\\ghost-right-odskok@2x.png";
+const std::string holePath = "..\\x64\\Debug\\data\\ghost-darkness@2x.png";
+const std::string standardPlatformPath = "..\\x64\\Debug\\data\\bck@2x.png";
